d46e6bc1c9a1da1fa3cff5d1482bdb1e631e4fc3
axel sanchez <20253ds084@utez.edu.mx>
2026-01-15 19:50:24 -0600

    Union de los archivos que tengo en features hechos en clase a main para empezar la tarea

--- BuenasPracticas/src/Main.java
+++ BuenasPracticas/src/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static double DESCUENTO = 0.10;
 
     public static void main(String[] args) {
-//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-// to see how IntelliJ IDEA suggests fixing it.
+
         Scanner sc = new Scanner(System.in);
         double subtotal = obtenerDouble(sc, "Dame el subtotal de la compra");
         double total = calcularTotal(subtotal);
