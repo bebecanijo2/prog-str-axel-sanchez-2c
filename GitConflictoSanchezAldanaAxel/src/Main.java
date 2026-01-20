8565f6229e3192ac4886f4731e62530472cd61b9
axel sanchez <20253ds084@utez.edu.mx>
2026-01-19 22:10:18 -0600

    feat: agregar descuento por umbral

--- GitConflictoSanchezAldanaAxel/src/Main.java
+++ GitConflictoSanchezAldanaAxel/src/Main.java
@@ -2,17 +2,38 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static double DESCUENTO = 0.10;
+    public static double UMBRAL_DESCUENTO = 1000.0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         double subtotal = pedirDouble(sc, "Subtotal: ");
-        double total = subtotal;
-
+        double total = aplicarDescuentoSiAplica(subtotal, UMBRAL_DESCUENTO);
         System.out.printf("Total a pagar: %.2f%n", total);
+
+
     }
 
-    public static double pedirDouble(Scanner scanner, String mensaje) {
+    /**
+     * Escaneo y leida de todos los double del programa
+     *
+     * @param sc      Le damos la funcion de scanner para que pueda leer el input del usuario
+     * @param mensaje Este parametro nos ayuda a ahorrarnos escribir un print
+     * @return Va a devolver el double que introdujo el usuario escaneado
+     */
+    public static double pedirDouble(Scanner sc, String mensaje) {
         System.out.print(mensaje);
-        return scanner.nextDouble();
+        return sc.nextDouble();
+    }
+
+    public static double aplicarDescuentoSiAplica(double subtotal, double total) {
+        if (subtotal > UMBRAL_DESCUENTO) {
+             total = subtotal - (subtotal * DESCUENTO);
+        } else if (subtotal <= UMBRAL_DESCUENTO) {
+            total = subtotal;
+        }
+        return total;
+
     }
 }
