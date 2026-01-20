32b5e00a45e4f7ab28f9aaf9eb3edc5e206cd812
axel sanchez <20253ds084@utez.edu.mx>
2026-01-19 21:10:18 -0600

    feat: agregar calculo de iva

--- GitConflictoSanchezAldanaAxel/src/Main.java
+++ GitConflictoSanchezAldanaAxel/src/Main.java
@@ -2,17 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static double IVA = 0.16;
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         double subtotal = pedirDouble(sc, "Subtotal: ");
-        double total = subtotal;
-
+        double total = sumaTotal(subtotal,IVA);
         System.out.printf("Total a pagar: %.2f%n", total);
     }
 
-    public static double pedirDouble(Scanner scanner, String mensaje) {
+    /**
+     * Escaneo y leida de todos los double del programa
+     * @param sc Le damos la funcion de scanner para que pueda leer el input del usuario
+     * @param mensaje Este parametro nos ayuda a ahorrarnos escribir un print
+     * @return Va a devolver el double que introdujo el usuario escaneado
+     */
+    public static double pedirDouble(Scanner sc, String mensaje) {
         System.out.print(mensaje);
-        return scanner.nextDouble();
+        return sc.nextDouble();
+    }
+
+    /**
+     * Suma del precio con el IVA incluido
+     * @param subtotal Es la cantidad original, el valor del producto a comprar
+     * @param total Es la cantidad original sumada al IVA
+     * @return Nos va a devolver la cantidad ya calculada
+     */
+    public static double sumaTotal(double subtotal, double total){
+        return total = subtotal+(subtotal*IVA);
     }
 }
