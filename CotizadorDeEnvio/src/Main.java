717bf1918e420d6f9dc3e93482b0567566f719d3
axel sanchez <20253ds084@utez.edu.mx>
2026-02-05 22:39:03 -0600

    Tarea CotizadorDeEnvio completada

--- CotizadorDeEnvio/src/Main.java
+++ CotizadorDeEnvio/src/Main.java
@@ -7,17 +7,20 @@ public class Main {
         InputValidator inputvalidator = new InputValidator();
         ShippingCalculator shippingCalculator = new ShippingCalculator();
         Ticket ticket = new Ticket();
-
+        //IPO
+        //I
         double pesoKg= inputvalidator.leerDoubleEnRango("Ingresa el peso en kg", sc, 0.1, 50.0);
         int distanciaKm = inputvalidator.leerIntEnRango("Ingresa la distancia en km", sc, 1, 2000);
         int tipoServicio = inputvalidator.leerIntEnRango("Ingresa el tipo de servicio 1)Estandar  2)Express", sc, 0, 3);
         boolean esZonaRemota = inputvalidator.leerBoolean("El destino esta en zona remota? true/false", sc);
 
+        //P
         double subtotal = shippingCalculator.calcularSubtotal(pesoKg, distanciaKm, tipoServicio, esZonaRemota);
         double iva = shippingCalculator.calcularIVA(subtotal);
         double total = shippingCalculator.calcularTotal(subtotal, iva);
 
 
+        //O
         ticket.imprimirTicket(pesoKg, distanciaKm, esZonaRemota, subtotal, total);
 
 
