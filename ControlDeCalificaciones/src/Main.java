4974afb9b4912536e8b29dde66e829da67b8b2e6
axel sanchez <20253ds084@utez.edu.mx>
2026-02-08 21:13:54 -0600

    Control de calificaciones

--- ControlDeCalificaciones/src/Main.java
+++ ControlDeCalificaciones/src/Main.java
@@ -6,7 +6,8 @@ public class Main{
         GradeService gradeService = new GradeService();
         InputValidator inputValidator = new InputValidator();
         Reporte reporte = new Reporte();
-
+        //IPO
+        //I
         String nombreDelAlumno = inputValidator.leerTextoNoVacio("Cual es tu nombre?: ", sc);
         int asistencia = inputValidator.leerIntEnRango("Con cuantas asistencias cuentas? 0-100 :", sc, 0, 100);
         boolean entregaProyecto = inputValidator.leerBoolean("Entregaste proyecto? true/false :", sc);
@@ -14,10 +15,12 @@ public class Main{
         double parcial2 = inputValidator.leerDoubleEnRango("Cuanto sacaste en el parcial 2?: 0-10.0", sc, 0, 10.0);
         double parcial3 = inputValidator.leerDoubleEnRango("Cuanto sacaste en el parcial 3?: 0-10.0", sc, 0, 10.0);
 
+        //P
         double promedioParciales = gradeService.calcularPromedioParciales(parcial1, parcial2, parcial3);
         double calificacionFinal = gradeService.calcularCalificacionFinal(asistencia, promedioParciales);
         String estado = reporte.determinarEstado(calificacionFinal, asistencia, entregaProyecto);
 
+        //O
         reporte.imprimirReporte(nombreDelAlumno, asistencia, promedioParciales, calificacionFinal, entregaProyecto, estado);
 
     }
