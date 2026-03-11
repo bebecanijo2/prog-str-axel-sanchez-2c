56e29c56e155583070d9b6681bb5fb3628314c40
AngelicaDavalos <20253rd007@utez.edu.mx>
2026-03-11 09:49:12 -0600

    edadDemoFile

--- demoListViewFile/src/main/java/com/example/demolistviewfile/services/PersonService.java
+++ demoListViewFile/src/main/java/com/example/demolistviewfile/services/PersonService.java
@@ -38,5 +38,16 @@ public class PersonService {
         if (em.isBlank() || !em.contains("@") || !em.contains(".")){
             throw new IllegalArgumentException("El email es invalido");
         }
+
+        try {
+            int edadValida = Integer.parseInt(edad);
+            if (edadValida < 0) throw new IllegalArgumentException("Edad menor a 0");
+            if (edadValida < 18) throw new IllegalArgumentException("Menor de 18");
+        } catch (Exception e) {
+            if (e instanceof NumberFormatException) {
+                throw new IllegalArgumentException("Edad no numérica");
+            }
+            throw e;
+        }
     }
 }
